fd0537c5958dc0e256735d1b4fb903a47ada8158
Alex Nweke <nwekealexchinedu@gmail.com>
2021-04-07 06:54:51 +0100

    added morgan to the project

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
   res.send('Hello from my Library App');
