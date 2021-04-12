d58ac101ebe833d796e354dce537493840717ce5
Alex Nweke <nwekealexchinedu@gmail.com>
2021-04-12 21:46:06 +0100

    used path to render the path of the html file

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
 app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.listen(8080, () => {
