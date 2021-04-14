ef7f02e2d6ae2d8e83a61020a5819d7399c4f439
Alex Nweke <nwekealexchinedu@gmail.com>
2021-04-14 22:37:03 +0100

    created a public directory and tell express to make it public

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');
 const app = express();
 
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
